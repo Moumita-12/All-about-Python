f2b73b71d0d6f3344254a287d00e89e7c226c5ce
Moumita Shib <120558169+Moumita-12@users.noreply.github.com>
2023-10-13 22:37:20 +0600

    Create string example 2.py

---
+++ string example 2.py
@@ -0,0 +1,22 @@
+name = "Tony Stark"
+print(name.upper())
+print(name.lower())
+
+
+
+print(name.find('o'))
+print(name.find('N'))
+print(name.find("Stark"))
+print(name.find("stark"))
+print(name.replace("Tony Stark", "Ironman"))
+print(name.replace("T", "M"))
+
+
+
+print(name)
+
+
+print("tony" in name)
+print("Stark" in name)
+print("m" in name)
+print("n" in name)
